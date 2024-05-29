55d9ee270a6b6f7ecf779f8aa2ff9e50daeb4981
agaaga823 <113017632+agaaga823@users.noreply.github.com>
2024-05-29 13:31:52 +0700

    Add files via upload

---
+++ Tugas 5/resources/views/datadiri.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Diri</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: lightpink;
+
+        }
+        .container {
+            width: 30%;
+            padding: 20px;
+            margin: 0 auto;
+            margin-top: 50px;
+            border-radius: 4px;
+            background-color: white;
+            box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        }
+        h1 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Data Diri</h1>
+        <p><b>Nama : </b>Siti Sarah</p>
+        <p><b>NPM : </b>222310019</p>
+        <p><b>Program Studi : </b>S1 Teknologi Informasi</p>
+        <p><b>Umur : </b>20 Tahun</p>
+        <p><b>Jenis Kelamin : </b>Perempuan</p>
+        <p><b>Agama : </b>Islam</p>
+        <p><b>Alamat : </b>Kp.Muara Tajur Rt.01/09 Sindangrasa</p>
+    </div>
+</body>
+</html>
